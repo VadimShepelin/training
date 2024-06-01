c146c1f416096ca435524804cb69c1e723f56bc8
Shepelin Vadim <kocichca@bk.ru>
2024-06-01 22:46:42 +0300

    new .java file

---
+++ .java
@@ -0,0 +1,11 @@
+public class Pac
+{
+    public static void main(String[] args) {
+        loxGey(4);
+        System.out.println(1);
+        System.out.println(2);
+
+
+        System.out.println(1);
+    }
+}
