0ec6da94eea1183d8dbdf658e699e60fedffd753
Вадим <kocichca@bk.ru>
2024-06-01 23:17:02 +0300

    Update .java

--- .java
+++ .java
@@ -1,11 +1,8 @@
 public class Pac
 {
     public static void main(String[] args) {
-        loxGey(4);
         System.out.println(1);
         System.out.println(2);
-
-
-        System.out.println(1);
+        System.out.println(3);
     }
 }
